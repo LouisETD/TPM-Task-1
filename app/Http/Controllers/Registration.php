3560ee788c02201fd244d9021f3526471cab7a4d
LouisETD <louisetd18@gmail.com>
2023-12-15 10:51:10 +0700

    second commit

---
+++ app/Http/Controllers/Registration.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Tenant;
+
+class registration extends Controller
+{
+    public function redirectToregister(){
+        return view('registration');
+    }
+
+    public function Registration(Request $request) {
+       
+        Tenant::create([
+            'nama_tenant' => $request-> nama_tenant_input,
+            'tipe_makanan' => $request-> tipe_makanan_input,
+            'asal_makanan' => $request-> asal_makanan_input
+        ]);
+       return redirect('/');
+    }
+}
