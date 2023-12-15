a848971fb0c20c2264d47f7ddee7c5106d821c7f
LouisETD <louisetd18@gmail.com>
2023-12-15 11:11:28 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,13 +34,14 @@
         <td>{{ $tenant->tipe_makanan }}</td>
         <td>{{ $tenant->asal_makanan }}</td>
         </tr>
-  </tbody>
-</table>
-    </div>
+    </tbody>
+    </table>
+  </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
     </script>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
         integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
     </script>
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ use App\Http\Controllers\registration;
 Route::get('/', [HomeController::class, 'home']);
 
 // Page Registration
-Route::get('/registration', [registration::class,'redirectToregister']);
+Route::get('/registration', [RegistrationController::class,'redirectToregister']);
 
 // Page Database
-Route::post('/post-registration', [registration::class, 'Registration']);
+Route::post('/post-registration', [RegistrationController::class, 'Registration']);
 
